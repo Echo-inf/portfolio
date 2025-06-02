9ac03c2f772aec9210a051c35ceeabff4f24b53e
Echo <lemonsdelicious@gmail.com>
2025-06-02 17:33:49 +0800

    Everything possibly edited

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var skilllink = document.getElementById("skill-link");
-var skillcontent = document.getElementById("skill-contents");
+var skilllinks = document.getElementsByClassName("skill-link");
+var skillcontents = document.getElementsByClassName("skill-contents");
 
 function opentab(tabname) {
-    
+    for (skilllink of skilllinks) {
+        skilllink.classList.remove("active-link");
+    }
+    for (skillcontent of skillcontents) {
+        skillcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
